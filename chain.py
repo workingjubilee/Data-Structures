ac109568830821eef96453349ed3e049ab34b955
workingjubilee <workingjubilee@gmail.com>
2019-06-10 14:14:26 -0700

    Starting to sketch Doubly Linked List

---
+++ chain.py
@@ -0,0 +1,10 @@
+class Link:
+    def __init__(self, value, next=None):
+        self.value =
+        self.next =
+
+
+class Chain:
+    def __init__(self):
+        self.head =
+        self.tail =
