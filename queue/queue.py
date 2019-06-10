862617f4bd080341d70c53974f21f7e59c51a770
workingjubilee <workingjubilee@gmail.com>
2019-06-10 14:11:25 -0700

    Super naive implementation of a queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,25 @@
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
+        # super naive first pass implementation!
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
+        return
 
-  def len(self):
-    pass
+    def dequeue(self):
+        if len(self.storage) > 0:
+            return self.storage.pop(0)
+        else:
+            return None
+
+    def len(self):
+        return len(self.storage)
+
+     # * Should have the methods: `enqueue`, `dequeue`, and `len`.
+     # * enqueue should add an item to the back of the queue.
+     # * dequeue should remove and return an item from the front of the queue.
+     # * len returns the number of items in the queue.
